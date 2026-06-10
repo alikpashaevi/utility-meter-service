2ab624b93b8446e0bfe81c431ff5cca4b57dfc7c
alikpashaevi <alikpashaev06@gmail.com>
2026-06-10 21:47:10 +0400

    infra: setup docker-compose infrastructure, database schemas, and liquibase base

---
+++ src/main/java/alik/utilitymeter/UtilityMeterApplication.java
@@ -0,0 +1,13 @@
+package alik.utilitymeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityMeterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UtilityMeterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/alik/utilitymeter/UtilityMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package alik.utilitymeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityMeterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
